2f61afad1e2d2c2ed19994ee9a3555717f7fdcf3
Pierre EVEN <evenpierre@orange.fr>
2020-12-15 20:46:10 +0100

    tests

--- Sources/PureHeaderTool/PureHeaderTests/Private/Main.cpp
+++ Sources/PureHeaderTool/PureHeaderTests/Private/Main.cpp
@@ -6,6 +6,52 @@
 #include "Reflection/RFunction.h"
 #include "Reflection/RConstructor.h"
 
+
+
+
+
+
+struct RTestFunction {
+
+	template<typename Return, typename Class, typename... Arguments>
+	using RFunctionPtr = std::function<Return (Class, Arguments...)>;
+
+	/**
+	 * Register new constructor
+	 */
+	template<typename Return, typename Class, typename... Arguments>
+	inline static RTestFunction* MakeFunction(const RFunctionPtr<Return, Class, Arguments...>& Function) {
+		return new RTestFunction(std::make_any<RFunctionPtr<Return, Class, Arguments...>>(Function));
+	}
+
+	/**
+	 * Instantiate new object
+	 */
+	template<typename Return, typename Class, typename... Arguments>
+	inline Return Execute(Arguments&&... inArguments) {
+
+		// Are arguments valids
+		if (const InstantiateFunc<Arguments...>* Factory = std::any_cast<InstantiateFunc<Arguments...>>(&ConstructorFunction)) {
+
+			// Call instantiate func
+			return (*Factory) (std::forward<Arguments>(inArguments)...);
+		}
+
+		return (*Factory) (std::forward<Arguments>(inArguments)...);
+
+		// Wrong arguments, return null
+		return nullptr;
+	}
+
+private:
+
+	RTestFunction(const std::any& inFunction)
+		: Function(inFunction) {}
+
+	const std::any Function;
+};
+
+
 int main() {
 	/**
 	 * Class tests
@@ -37,4 +83,13 @@ int main() {
 	RFunction<double, ChildOneTwo, int, int, int>* FuncB = MyClass->GetFunction<double, ChildOneTwo, int, int, int>("FunctionB");
 	LOG(FuncA->GetName()); FuncA->Execute((ChildOneTwo*)MyObject);
 	LOG(FuncB->GetName() + " : " + FuncB->Execute((ChildOneTwo*)MyObject, 10, 20, 30));
+
+
+
+	std::function<double(ChildOneTwo*, int, int, int)> test = &ChildOneTwo::FunctionB;
+	
+	double result = test((ChildOneTwo*)MyObject, 1, 2, 3);
+	LOG(result);
+
+	//test->Execute<void, ChildOneTwo, int, int, int>(this, );
 }
